4e048ffc861b7bf7b2d0cf2116cf23438b52f6e0
Alan Webb <knogeek@gmail.com>
2016-02-24 16:10:12 -0500

    GRRRR! Changes to master did not show up here.

--- Resources/index.php
+++ Resources/index.php
@@ -7,51 +7,34 @@
 </head>
 <body>
     <!-- Display all listed agencies and affiliated services with primary phone numbers. -->
-    <p style="text-align:center; font-weight:700">Agencies and Services</p>
-    <p style="#body #p.left">Type one or more search values in the form below then click Filter.</p>
     <form name="Search" method="get" action="search.php">
         <fieldset>
-            <legend><strong>Filter/Find Agencies and Services Offered</strong></legend>
-            <label>Service Area:</label>
+            <legend><strong>What Are You Looking For?</strong></legend>
             <!-- Select items are static until I learn how to populate it from the source data -->
-                <select name="servicearea">
-                    <option value="richmond">Richmond</option>
-                    <option value="henrico">Henrico</option>
-                    <option value="chesterfield">Chesterfield</option>
-                    <option value="hanover">Hanover</option>
-                    <option value="tri-cities">Tri-Cities</option>
-                </select><br>
-            <label>Agency Name:</label><input type="search" name="agencyname"><br>
-            <label>City:</label><input type="search" name="city">
-                <label>State:</label><select name="state">
-                    <option value="va">Virginia</option>
-                    <option value="md">Maryland</option>
-                    <option value="sc">South Carolina</option>
-                    <option value="nc">North Carolina</option>
-                </select><br>
-            <label>Postal Code:</label><input type="search" name="postalcode"><br>
-            <input type="submit" value="Filter">
+            <input type="search" name="searchterm"><br>
+            <input type="submit" value="Find">
         </fieldset>
     </form>
     <!--Table is a static representation of expected result-->
     <table>
-        <thead>Agency Phone Numbers</thead>
+        <thead>Service</thead>
         <tbody>
             <tr>
-                <td>Phone #:</td>
-                <td>Type of Phone #:</td>
-                <td>Primary #?</td>
-                <td>Note</td>
+                <td>Food</td>
+                <td>(25)</td>
             </tr>
             <tr>
-                <td>757-456-2366 </td>
-                <td>Office</td>
-                <td>No</td>
-                <td>Senior Citizen Issues</td>
+                <td>Shelter</td>
+                <td>(4)</td>
+            </tr>
+            <tr>
+                <td>Mental Health</td>
+                <td>(2)</td>
             </tr>
         </tbody>
     </table>
-    <div class=footer"">Copyright 2016, Alan Webb, All Rights Reserved</div>
+    <img src="panic_button.jpg" alt="Panic Button">
+    <div class="footer">Copyright 2016, Alan Webb, All Rights Reserved. Thank you, Nicki Moon.</div>
     <?php
     /**
  * Display all listed agencies and affiliated services with primary phone numbers.
